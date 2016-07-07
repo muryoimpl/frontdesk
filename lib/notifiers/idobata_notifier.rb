b4b76016fa91b68dee1f1adeafebf02b2c121c2f
muryoimpl <muryoimpl@gmail.com>
2016-07-08 01:55:08 +0900

    post message to idobata

--- lib/notifiers/idobata_notifier.rb
+++ lib/notifiers/idobata_notifier.rb
@@ -3,14 +3,25 @@ require 'net/http'
 class FrontDesk::IdobataNotifier < FrontDesk::Notifier
   def enter
     puts 'enter idobata'
+    Net::HTTP.post_form URI.parse(Settings.idobata.url), source: source_of_entry,  format: :html
   end
 
   def leave
     puts 'leave idobata'
+    Net::HTTP.post_form URI.parse(Settings.idobata.url), source: source_of_exit,  format: :html
   end
 
   private
 
-  def http_client
+  def source_of_entry
+    <<~HTML
+      #{Settings.room.name} に着席しました。
+    HTML
+  end
+
+  def source_of_exit
+    <<~HTML
+      #{Settings.room.name} から離席しました。
+    HTML
   end
 end
