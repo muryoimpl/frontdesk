80b46086ae390058e23bf06fcd73a8d5965134f1
muryoimpl <muryoimpl@gmail.com>
2016-07-10 01:43:17 +0900

    setup distance sensor's pins

--- lib/work.rb
+++ lib/work.rb
@@ -3,6 +3,8 @@ class FrontDesk::Work
     def start
       NotifierRunner.require_notifiers
 
+      setup_distance_sensers
+
       PiPiper.watch pin: Settings.pins.infrared_sensor do
         read
 
@@ -12,10 +14,29 @@ class FrontDesk::Work
       PiPiper.wait
 
       at_exit {
-        [Settings.pins.infrared_sensor].each do |pin|
+        [
+          Settings.pins.infrared_sensor,
+          Settings.pins.distance_sensor.echo,
+          Settings.pins.distance_sensor.trig
+        ].each do |pin|
           File.open('/sys/class/gpio/unexport', 'w') {|f| f.write(pin.to_s) }
         end
       }
+    end
+
+    private
+
+    def setup_distance_sensers
+      distance_trig_pin
+      distance_echo_pin
+    end
+
+    def distance_trig_pin
+      @distance_trig_pin ||= PiPiper::Pin.new(Settings.pins.distance_sensor.trig, direction: :out)
+    end
+
+    def distance_echo_pin
+      @distance_echo_pin ||= PiPiper::Pin.new(Settings.pins.distance_sensor.echo, trigger: :rising)
     end
   end
 end
