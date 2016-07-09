a1e6e9d72e09cb527a9624d3574df6f138997738
muryoimpl <muryoimpl@gmail.com>
2016-07-10 01:45:57 +0900

    calculate distance between Raspberry Pi and a person who is sitting on

--- lib/notifier_runner.rb
+++ lib/notifier_runner.rb
@@ -1,38 +1,41 @@
 require 'yaml'
 
-class FrontDesk::NotifierRunner
-  class << self
-    def run(value)
-      FrontDesk::Notifier.subclasses.each do |notifier|
-        case value
-        when PiPiper::Pin::GPIO_HIGH
-          notifier.instance.enter
-        when PiPiper::Pin::GPIO_LOW
-          notifier.instance.leave
+module FrontDesk
+  class NotifierRunner
+    class << self
+      def run(value)
+        FrontDesk::Notifier.subclasses.each do |notifier|
+          next unless use?(notifier)
+
+          case value
+          when PiPiper::Pin::GPIO_HIGH
+            notifier.instance.enter
+          when PiPiper::Pin::GPIO_LOW
+            notifier.instance.leave
+          end
         end
       end
-    end
 
-    def notifier_file_paths
-      @notifier_file_paths = [Pathname('./lib/notifiers/*.rb')]
-    end
-
-    def require_notifiers
-      Dir[*notifier_file_paths].each do |f|
-        next unless use?(notifier)
-
-        require f
+      def notifier_file_paths
+        @notifier_file_paths = [Pathname('./lib/notifiers/*.rb')]
+        # @notifier_file_paths = [Pathname(root).join('notifiers', '*.rb')]
       end
-    end
 
-    private
+      def require_notifiers
+        Dir[*notifier_file_paths].each do |f|
+          require f
+        end
+      end
 
-    def use?(notifier)
-      using_notifiers.include?(notifier.to_s)
-    end
+      private
 
-    def using_notifiers
-      Settings.notifiers
+      def use?(notifier)
+        using_notifiers.include?(notifier.to_s)
+      end
+
+      def using_notifiers
+        Settings.notifiers
+      end
     end
   end
 end
